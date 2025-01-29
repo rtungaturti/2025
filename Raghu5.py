75c364ed5f411a42b8405b0fee24ea1d3807acc5
Raghu1976LLB <rtungaturti@gmail.com>
2025-01-29 14:19:35 +0530

    Initial commit5

--- Raghu5.py
+++ Raghu5.py
@@ -27,7 +27,7 @@ def test_login(browser):
     browser.goto("https://practicetestautomation.com/practice-test-login/")
     example_page = ExamplePagePlaywright(browser)
     example_page.login("student", "Password1234")
-    print("Hi3asa")
+    print("Hi3asahhh")
     # Add assertions to verify login success
     assert "Logged In Successfully" in browser.content()  # Use correct Playwright command
 
